9ab233e68f6324411bbd0b18c716e6c04c1ea68e
Fishlennon <jpaviles5@gmail.com>
2024-05-09 00:20:30 -0400

    tailwind

--- src/app/_components/about.tsx
+++ src/app/_components/about.tsx
@@ -3,8 +3,22 @@
 export default function About () {
     return(
         <div id="about" className="flex flex-wrap ">
-            <img className="w-1/3 p-2" src="https://th.bing.com/th/id/OIG2._06gypuZoIc8pYeTh7b.?pid=ImgGn" alt="" />
-            <img className="w-1/3 p-2" src="https://th.bing.com/th/id/OIG2._06gypuZoIc8pYeTh7b.?pid=ImgGn" alt="" />
+            <div className="flex flex-row">
+            <p>qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed 
+            qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed 
+            qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed 
+            qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed 
+            qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed 
+            qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed 
+            qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed 
+            qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed 
+            qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed
+            qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed 
+            qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed 
+            qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed qazed</p>
+            <img className="w-1/2 p-2" src="https://th.bing.com/th/id/OIG2._06gypuZoIc8pYeTh7b.?pid=ImgGn" alt="" />
+
+            </div>
             <img className="w-1/3 p-2" src="https://th.bing.com/th/id/OIG2._06gypuZoIc8pYeTh7b.?pid=ImgGn" alt="" />
             <img className="w-1/3 p-2" src="https://th.bing.com/th/id/OIG2._06gypuZoIc8pYeTh7b.?pid=ImgGn" alt="" />
             <img className="w-1/3 p-2" src="https://th.bing.com/th/id/OIG2._06gypuZoIc8pYeTh7b.?pid=ImgGn" alt="" />
